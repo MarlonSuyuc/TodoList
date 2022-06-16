5b49c01c350da95827491e61c552da8de3bdba41
marlonsuyuc <marlonsuyuc0@gmail.com>
2022-06-15 18:33:01 -0600

    avances en todo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,77 @@
 const d = document,
+  list = d.querySelector(".todo ul"),
   input = d.querySelector(".form input"),
   btnAdd = d.querySelector(".btn--add"),
   btnremove = d.querySelector(".btn--remove"),
-  btnclearAll = d.querySelector(".btn--clearAll");
+  btnclearAll = d.querySelector(".btn--clearAll"),
+  template = d.querySelector("#template"),
+  fragment = d.createDocumentFragment();
+let tareas = [];
 
 d.addEventListener("click", (e) => {
   if (e.target.matches(".btn--add")) {
-    alert("aaaaaaaaaaaaaaaaa");
+    agregartarea();
   }
   if (e.target.matches(".btn--remove")) {
-    alert("bbbbbbbbbbbbbbb");
   }
   if (e.target.matches(".btn--clearAll")) {
-    alert("ccccccccccccccc");
   }
 });
+
+const agregartarea = () => {
+  const tarea = input.value;
+  
+  if (tarea === "") {
+    error("Ingrese una tarea");
+    return;
+  }
+
+  const objTareas = {
+    id: Date.now(),
+    titulo: tarea,
+  };
+  tareas = [...tareas, objTareas];
+  input.value = "";
+  pintarTareas();
+};
+
+const pintarTareas = () => {
+  list.textContent = "";
+  tareas.map((item) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector(".text--li").textContent = item.titulo;
+
+    const button = d.createElement("button");
+    button.classList.add("btn", "btn--remove");
+    button.textContent = "-";
+    clone.querySelector(".text--li").appendChild(button);
+    fragment.appendChild(clone);
+  });
+  list.appendChild(fragment);
+  LocalStorage();
+};
+
+// conexion con localStorage
+const LocalStorage = () => {
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+};
+
+// mostrar las tareas al inicio de la app
+const eventListeners = () => {
+  d.addEventListener("DOMContentLoaded", () => {
+    tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    pintarTareas();
+  });
+};
+eventListeners();
+
+const error = (message) => {
+  const li = document.createElement("li");
+  li.textContent = message;
+  li.classList.add("error");
+  list.insertAdjacentElement("afterbegin", li);
+  // beforeend
+  // beforebegin
+  // afterend
+  setTimeout(() => li.remove(), 1000);
+};
